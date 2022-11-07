c52f333ad83dc0f5868c85f29527d5b73cfd70a4
Gregori-Rivera <gregori.rivera@gmail.com>
2022-11-07 21:03:33 +0000

    color de pinta aleatorio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,61 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const randomNumber = () => {
+  let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  return numbers[Math.floor(Math.random() * numbers.length)];
+};
+
+const changeNumber = p => {
+  let newNumber = randomNumber();
+  while (newNumber == p.innerHTML) {
+    newNumber = randomNumber();
+  }
+  return newNumber;
+};
+
+const randomPinta = () => {
+  let pintas = ["♠", "♦", "♣", "♥"];
+  return pintas[Math.floor(Math.random() * pintas.length)];
+};
+
+const changePinta = p => {
+  let newPinta = randomPinta();
+  while (newPinta == p.innerHTML) {
+    newPinta = randomPinta();
+  }
+  return newPinta;
+};
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = document.querySelectorAll(".number");
+  let p;
+  let randomNumberText = randomNumber();
+  for (let number of numbers) {
+    p = document.createElement("p");
+    p.innerHTML = randomNumberText;
+    number.innerHTML = "";
+    number.appendChild(p);
+  }
+
+  {
+    let pintas = document.querySelectorAll(".pintaheader, .pintafooter");
+    let p;
+    let randomPintaPicture = randomPinta();
+    for (let pinta of pintas) {
+      console.log(pinta);
+      if (randomPintaPicture == "♥" || randomPintaPicture == "♦") {
+        document
+          .querySelectorAll(".pintaheader, .pintafooter")
+          .forEach(element => {
+            element.classList.add("rojo");
+          });
+      }
+
+      p = document.createElement("p");
+      p.innerHTML = randomPintaPicture;
+      pinta.innerHTML = "";
+      pinta.appendChild(p);
+      console.log(p);
+    }
+  }
 };
